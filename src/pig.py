220b5598078a0d2a104f20c4ee3ee552f6bc8380
John Hunt <john.hunt10@gmail.com>
2024-05-07 17:31:26 +0100

    Added additional functions and close tabs

--- src/pig.py
+++ src/pig.py
@@ -1,9 +1,8 @@
 from constants import *
 import tkinter as tk
-from tkinter import ttk, Label, messagebox
+from tkinter import ttk, Label, messagebox, simpledialog
 from PIL import ImageTk, Image, ImageOps, ImageFilter
 from utils import select_filename
-from collections import namedtuple
 
 class TabLabel(Label):
     def __init__(self, parent, photo_image=None, image=None, filename=None):
@@ -41,6 +40,7 @@ class PIGEditorController:
         self.tab_views = []
         self.tab_counter = 1
 
+
     def load_image(self):
         filename = select_filename()
         if filename != '':
@@ -64,12 +64,35 @@ class PIGEditorController:
     def invert_image(self):
         self._apply_simple_image_operation(ImageOps.invert, 'invert')
 
+    def colorize_image(self):
+        selected_tab_view = self.editor.get_current_tab_view()
+        selected_tab_image = selected_tab_view.image
+        if selected_tab_image.mode == "L":
+            args = ('blue', 'white')
+            self._apply_simple_image_operation(ImageOps.colorize, 'colorize', args)
+        else:
+            messagebox.showerror("Error", "Colorize can only be applied to greyscale images")
+
     def posterize_image(self):
         args=1
+        answer = simpledialog.askinteger(title="Bits Channel Input",
+                                         prompt="The number of bits to keep for each channel (1-8).",
+                                         initialvalue=1,
+                                         parent=self.root,
+                                         minvalue=1, maxvalue=8)
+        if answer is not None:
+            args = answer
         self._apply_simple_image_operation(ImageOps.posterize, 'posterize', args=args)
 
     def solarize_image(self):
         args = 128
+        answer = simpledialog.askinteger(title="Thrershold Input",
+                                         prompt="Value threshold value for solarize? \n(All pixels above this grayscale level are inverted)",
+                                         initialvalue=128,
+                                         parent=self.root,
+                                         minvalue=1, maxvalue=256)
+        if answer is not None:
+            args = answer
         self._apply_simple_image_operation(ImageOps.solarize, 'solarize', args=args)
 
     def find_edges_in_image(self):
@@ -80,7 +103,6 @@ class PIGEditorController:
         image_with_edges = selected_tab_image.filter(ImageFilter.FIND_EDGES)
         text_label = 'edges' + str(self.tab_counter)
         tab_view = TabView(self.editor.tabbed_view, image=image_with_edges, text_label=text_label)
-        self.tab_views.append(tab_view)
         self.tab_counter = self.tab_counter + 1
 
     def _apply_simple_image_operation(self, image_op=None, type_of_op='', args=None):
@@ -90,14 +112,35 @@ class PIGEditorController:
         # applying image operation
         if args is None:
             new_image = image_op(selected_tab_image)
+        elif isinstance(args, list) or isinstance(args, tuple):
+            if len(args) == 1:
+                new_image = image_op(selected_tab_image, args[0])
+            elif len(args) == 2:
+                new_image = image_op(selected_tab_image, args[0], args[1])
+            elif len(args) == 3:
+                new_image = image_op(selected_tab_image, args[0], args[1], args[3])
         else:
             new_image = image_op(selected_tab_image, args)
         # Display greyscale image in a new tab
         text_label = type_of_op + str(self.tab_counter)
+        # Create tab for tabbed view
         tab_view = TabView(self.editor.tabbed_view, image=new_image, text_label=text_label)
         self.tab_views.append(tab_view)
         self.tab_counter = self.tab_counter + 1
 
+
+    def on_tab_click(self, event):
+        print('widget:', event.widget)
+        print('x:', event.x)
+        print('y:', event.y)
+
+        response = messagebox.askquestion('Delete Tab', 'Do you want to delete the tab?')
+        if response == 'yes':
+            nb = self.editor.tabbed_view
+            clicked_tab = nb.tk.call(nb._w, "identify", "tab", event.x, event.y)
+            print('clicked tab:', clicked_tab)
+            nb.forget(clicked_tab)
+            del self.tab_views[clicked_tab]
 
     def exit_app(self):
         self.root.quit()
@@ -109,7 +152,6 @@ class PIGMenuBar(tk.Menu):
         self.controller = controller
         self.create_file_menu()
         self.create_image_menu()
-        self.create_edit_menu()
 
     def create_file_menu(self):
         file_menu = tk.Menu(self, tearoff=0)
@@ -120,19 +162,18 @@ class PIGMenuBar(tk.Menu):
     def create_image_menu(self):
         image_menu = tk.Menu(self, tearoff=0)
         image_menu.add_command(label='Greyscale Image', command=self.controller.make_greyscale_image)
+        image_menu.add_separator()
+        image_menu.add_command(label='Flip Image', command=self.controller.flip_image)
+        image_menu.add_command(label='Mirror Image', command=self.controller.mirror_image)
+        image_menu.add_command(label='Invert Image', command=self.controller.invert_image)
+        image_menu.add_separator()
         image_menu.add_command(label='Auto Contrast', command=self.controller.apply_auto_contrast)
         image_menu.add_command(label='Find edges', command=self.controller.find_edges_in_image)
+        image_menu.add_separator()
+        image_menu.add_command(label='Colorize Image', command=self.controller.colorize_image)
+        image_menu.add_command(label='Posterize Image', command=self.controller.posterize_image)
+        image_menu.add_command(label='Solarize Image', command=self.controller.solarize_image)
         self.add_cascade(label='Image', menu=image_menu)
-
-    def create_edit_menu(self):
-        edit_menu = tk.Menu(self, tearoff=0)
-        edit_menu.add_command(label='Flip Image', command=self.controller.flip_image)
-        edit_menu.add_command(label='Mirror Image', command=self.controller.mirror_image)
-        edit_menu.add_command(label='Invert Image', command=self.controller.invert_image)
-        edit_menu.add_separator()
-        edit_menu.add_command(label='Posterize Image', command=self.controller.posterize_image)
-        edit_menu.add_command(label='Solarize Image', command=self.controller.solarize_image)
-        self.add_cascade(label='Edit', menu=edit_menu)
 
 
 class PIGEditor:
@@ -151,6 +192,11 @@ class PIGEditor:
 
         # Set up the controller
         self.controller = PIGEditorController(self, self.root)
+
+        # Set up menu handling on tabs
+        # bind function to notebook
+        self.tabbed_view.bind('<Button-3>', self.controller.on_tab_click)
+        self.tabbed_view.bind('<Button-2>', self.controller.on_tab_click) # for mac users
 
         # Set up menus
         self.menubar = PIGMenuBar(self.root, self.controller)
