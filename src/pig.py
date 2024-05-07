7e80e321f5b97d4f9be4b6390246907321543d42
John Hunt <john.hunt10@gmail.com>
2024-05-07 12:25:55 +0100

    Added additional simple image processing functions

--- src/pig.py
+++ src/pig.py
@@ -1,9 +1,9 @@
 from constants import *
 import tkinter as tk
 from tkinter import ttk, Label, messagebox
-from PIL import ImageTk, Image, ImageOps
+from PIL import ImageTk, Image, ImageOps, ImageFilter
 from utils import select_filename
-
+from collections import namedtuple
 
 class TabLabel(Label):
     def __init__(self, parent, photo_image=None, image=None, filename=None):
@@ -33,26 +33,6 @@ class TabView:
             self.tabbed_view.add(self.label, text=text_label)
 
 
-
-class PIGMenuBar(tk.Menu):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        self.controller = controller
-        self.create_file_menu()
-        self.create_image_menu()
-
-    def create_file_menu(self):
-        file_menu = tk.Menu(self, tearoff=0)
-        file_menu.add_command(label='Load Image', command=self.controller.load_image)
-        file_menu.add_command(label='Exit', command=self.controller.exit_app)
-        self.add_cascade(label='File', menu=file_menu)
-
-    def create_image_menu(self):
-        image_menu = tk.Menu(self, tearoff=0)
-        image_menu.add_command(label='Greyscale Image', command=self.controller.make_greyscale_image)
-        self.add_cascade(label='Image', menu=image_menu)
-
-
 class PIGEditorController:
 
     def __init__(self, editor, root):
@@ -70,19 +50,89 @@ class PIGEditorController:
             messagebox.showerror("Error", "No file selected")
 
     def make_greyscale_image(self):
+        self._apply_simple_image_operation(ImageOps.grayscale, 'greyscale')
+
+    def apply_auto_contrast(self):
+        self._apply_simple_image_operation(ImageOps.autocontrast, 'auto contrast')
+
+    def flip_image(self):
+        self._apply_simple_image_operation(ImageOps.flip, 'flip')
+
+    def mirror_image(self):
+        self._apply_simple_image_operation(ImageOps.mirror, 'mirror')
+
+    def invert_image(self):
+        self._apply_simple_image_operation(ImageOps.invert, 'invert')
+
+    def posterize_image(self):
+        args=1
+        self._apply_simple_image_operation(ImageOps.posterize, 'posterize', args=args)
+
+    def solarize_image(self):
+        args = 128
+        self._apply_simple_image_operation(ImageOps.solarize, 'solarize', args=args)
+
+    def find_edges_in_image(self):
         # find currently selected tab
         selected_tab_view = self.editor.get_current_tab_view()
         selected_tab_image = selected_tab_view.image
-        # applying grayscale method
-        grey_scale_image = ImageOps.grayscale(selected_tab_image)
-        text_label = 'greyscale' + str(self.tab_counter)
-        tab_view = TabView(self.editor.tabbed_view, image=grey_scale_image, text_label=text_label)
+        # apply edge finding function
+        image_with_edges = selected_tab_image.filter(ImageFilter.FIND_EDGES)
+        text_label = 'edges' + str(self.tab_counter)
+        tab_view = TabView(self.editor.tabbed_view, image=image_with_edges, text_label=text_label)
+        self.tab_views.append(tab_view)
+        self.tab_counter = self.tab_counter + 1
+
+    def _apply_simple_image_operation(self, image_op=None, type_of_op='', args=None):
+        # find currently selected tab
+        selected_tab_view = self.editor.get_current_tab_view()
+        selected_tab_image = selected_tab_view.image
+        # applying image operation
+        if args is None:
+            new_image = image_op(selected_tab_image)
+        else:
+            new_image = image_op(selected_tab_image, args)
+        # Display greyscale image in a new tab
+        text_label = type_of_op + str(self.tab_counter)
+        tab_view = TabView(self.editor.tabbed_view, image=new_image, text_label=text_label)
         self.tab_views.append(tab_view)
         self.tab_counter = self.tab_counter + 1
 
 
     def exit_app(self):
         self.root.quit()
+
+
+class PIGMenuBar(tk.Menu):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.create_file_menu()
+        self.create_image_menu()
+        self.create_edit_menu()
+
+    def create_file_menu(self):
+        file_menu = tk.Menu(self, tearoff=0)
+        file_menu.add_command(label='Load Image', command=self.controller.load_image)
+        file_menu.add_command(label='Exit', command=self.controller.exit_app)
+        self.add_cascade(label='File', menu=file_menu)
+
+    def create_image_menu(self):
+        image_menu = tk.Menu(self, tearoff=0)
+        image_menu.add_command(label='Greyscale Image', command=self.controller.make_greyscale_image)
+        image_menu.add_command(label='Auto Contrast', command=self.controller.apply_auto_contrast)
+        image_menu.add_command(label='Find edges', command=self.controller.find_edges_in_image)
+        self.add_cascade(label='Image', menu=image_menu)
+
+    def create_edit_menu(self):
+        edit_menu = tk.Menu(self, tearoff=0)
+        edit_menu.add_command(label='Flip Image', command=self.controller.flip_image)
+        edit_menu.add_command(label='Mirror Image', command=self.controller.mirror_image)
+        edit_menu.add_command(label='Invert Image', command=self.controller.invert_image)
+        edit_menu.add_separator()
+        edit_menu.add_command(label='Posterize Image', command=self.controller.posterize_image)
+        edit_menu.add_command(label='Solarize Image', command=self.controller.solarize_image)
+        self.add_cascade(label='Edit', menu=edit_menu)
 
 
 class PIGEditor:
@@ -96,7 +146,7 @@ class PIGEditor:
         self.root.title(TITLE)
 
         # Set the tabbed display
-        self.tabbed_view = ttk.Notebook(self.root,width=800, height=600)
+        self.tabbed_view = ttk.Notebook(self.root, width=800, height=600)
         self.tabbed_view.pack(expand=True)
 
         # Set up the controller
@@ -109,17 +159,7 @@ class PIGEditor:
     def get_current_tab_view(self):
         # This code gets the currently selected tab
         selected_tab_id = self.tabbed_view.select()
-        print(selected_tab_id)
-        # tab = self.tabbed_view.tab(selected_tab_id, "text")
-        # print(tab)
-        # idx = self.tabbed_view.index(selected_tab_id)
-        # print(idx)
-        # current = self.tabbed_view.index("current")
-        # print(current)
-        # # tab = self.tabbed_view[str(current)]
-        # # print(tab)
         label = self.tabbed_view.nametowidget(selected_tab_id)
-        print(label)
         return label
 
     def mainloop(self):
